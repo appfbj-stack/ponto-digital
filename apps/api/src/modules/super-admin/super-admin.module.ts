635c616f6bfa81d84462dd502232ad069211aeb2
Mavis <Mavis@MiniMax>
2026-08-28 16:44:05 -0300

    fix(api): super-admin importa BillingModule

--- apps/api/src/modules/super-admin/super-admin.module.ts
+++ apps/api/src/modules/super-admin/super-admin.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SuperAdminService } from './super-admin.service';
 import { SuperAdminController } from './super-admin.controller';
+import { BillingModule } from '../billing/billing.module';
 
 @Module({
+  imports: [BillingModule],
   providers: [SuperAdminService],
   controllers: [SuperAdminController],
   exports: [SuperAdminService],
